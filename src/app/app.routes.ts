04624b27cac8c5cf00c51bed12f9b0b1637ec0ca
Allan Lopes <lopes.shigame@gmail.com>
2025-11-06 00:33:45 -0300

    Add rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Adm } from './adm/adm';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'login', component: Login},
+    {path: 'adm', component: Adm}
+];
