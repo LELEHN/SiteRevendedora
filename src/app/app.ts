e5b131d4c3bcf69f00084362fb21f7a10942e594
Leleh Nobre <lethicianobre2@gmail.com>
2025-11-06 00:10:43 -0300

    tudo prontin, basicamente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./home/home";
+import { Adm } from "./adm/adm";
+import { Login } from "./login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Adm, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
